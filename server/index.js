75d07d06f7185893de6f205da84f4fcb4731bf7f
shauryasds <shauryadeep589@gmail.com>
2025-02-26 11:59:33 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
    credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  exposedHeaders:['X-Total-Count']
 }));
 const  bodyParser = require('body-parser');
 const { stripeWebhook } = require('./controllers/orderController');
