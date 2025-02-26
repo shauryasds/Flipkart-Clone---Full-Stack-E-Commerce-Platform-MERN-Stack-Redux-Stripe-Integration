a980d68890da13824089eab74ab26aa0a048002f
shauryasds <119954061+shauryasds@users.noreply.github.com>
2025-02-26 20:52:50 +0530

    Update SlideShow.jsx

--- frontend/src/component/SlideShow.jsx
+++ frontend/src/component/SlideShow.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Slideshow({ slideImages }) {
   return (
@@ -14,12 +15,14 @@ export default function Slideshow({ slideImages }) {
       >
         {slideImages.map((slideImage, index) => (
           <div key={index} className="relative w-full h-full">
+            <Link to="/category/all" className="block">
             <img
               src={slideImage.url}
               alt={`Slide ${index + 1}`}
               className="w-full h-full object-cover object-center"
               loading="lazy"
             />
+            </Link>
           </div>
         ))}
       </Slide>
