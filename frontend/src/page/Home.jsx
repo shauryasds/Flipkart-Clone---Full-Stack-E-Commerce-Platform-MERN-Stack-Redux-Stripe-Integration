d38a15b21c1dcb83c04faa68671fe2cc614d5ea4
shauryasds <119954061+shauryasds@users.noreply.github.com>
2025-02-27 08:22:52 +0530

    Update Home.jsx

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -76,8 +76,18 @@ const Home = () => {
               Showing {products.length} of {productsCount} products
             </div>
           </div>
-          
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+          { products.length === 0 ? (
+            <div className="text-center mt-8">
+              <span className="flex items-center justify-center">
+                <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none"/>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                </svg>
+                Loading...
+              </span>
+            </div>
+          ) :
+          (<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
             {products.map((product) => (
               <Link 
                 to={`/product/${product._id}`} 
@@ -93,7 +103,7 @@ const Home = () => {
                 />
               </Link>
             ))}
-          </div>
+          </div>)
   
           {/* View More Button */}
           {products.length < productsCount && (
